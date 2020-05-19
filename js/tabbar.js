1d884fe9fe9f7a33493203c27f9e59a2a5c3fd77
KosmicArts <54221340+KosmicArts@users.noreply.github.com>
2020-05-19 15:40:50 -0500

    new tabbar default classes
    commented out js, added new tabbar classes to make tab content display: none on defaullt, while also adding an active version that does the opposite.

--- js/tabbar.js
+++ js/tabbar.js
@@ -1,37 +1,33 @@
-const setUpTabs = () => {
-    document.querySelectorAll(".tabbar__link").forEach(link => {
-        link.addEventListener("click", () => {
-            const tabbar = link.parentElement.parentElement;
-            const shopList = document.querySelector(".shop");
-            const tabNumber = link.dataset.forTab;
+// const setUpTabs = () => {
+//     document.querySelectorAll(".tabbar__link").forEach(link => {
+//         link.addEventListener("click", () => {
+//             const tabbar = link.parentElement.parentElement;
+//             const shopList = document.querySelector(".shop");
+//             const tabNumber = link.dataset.forTab;
 
-            const tabToActivate = shopList.querySelector(`li[data-tab="${tabNumber}"]`);
+//             const tabToActivate = shopList.querySelector(`li[data-tab="${tabNumber}"]`);
 
-            // HELP =============================
-            // CAN ONLY TOGGLE, DOESN'T WORK EXACTLY AS INTENDED
+//             // HELP =============================
+//             // CAN ONLY TOGGLE, DOESN'T WORK EXACTLY AS INTENDED
 
-            tabbar.querySelectorAll(`.tabbar__link--${tabNumber}`).forEach(link => {
-                link.classList.remove(`tabbar__link--${tabNumber}--active`);
-                link.classList.remove(`tabbar__link--${tabNumber}--active`);
-                link.classList.remove(`tabbar__link--${tabNumber}--active`);
+//             tabbar.querySelectorAll(`.tabbar__link--${tabNumber}`).forEach(link => {
+//                 link.classList.remove(`tabbar__link--${tabNumber}--active`);
+//                 link.classList.remove(`tabbar__link--${tabNumber}--active`);
+//                 link.classList.remove(`tabbar__link--${tabNumber}--active`);
                 
-            });
+//             });
 
-            shopList.querySelectorAll(`.shop__item--${tabNumber}`).forEach(shopItem => {
-                if(shopItem.classList.contains("shop__item--active")){
-                    shopItem.classList.remove("shop__item--active");
-                } else{
-                    shopItem.classList.add("shop__item--active");
-                }
-                
+//             shopList.querySelectorAll(`.shop__item--${tabNumber}`).forEach(shopItem => {
+//                 shopItem.classList.remove("shop__item--active");
 
-            });
+//             });
 
 
-        });
-    })
-}
+//         });
+//     })
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    setUpTabs();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     setUpTabs();
+// });
+
