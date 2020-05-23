cb5c9e5d82f89babab69a77e7daf7968c9762957
KosmicArts <54221340+KosmicArts@users.noreply.github.com>
2020-05-23 15:34:27 -0500

    tried another way
    the js is shorter, but it still doesnt show all the items i want when i go to click a tab >:(((

--- js/tabbar.js
+++ js/tabbar.js
@@ -1,53 +1,44 @@
-const setUpTabs = () => {
-    document.querySelectorAll(".tabbar__link").forEach(link => {
-        link.addEventListener("click", () => {
-            const tabbar = link.parentElement.parentElement;
-            const shopList = document.querySelector(".shop");
-            const tabNumber = link.dataset.forTab;
+// const setUpTabs = () => {
+//     document.querySelectorAll(".tabbar__link").forEach(link => {
+//         link.addEventListener("click", () => {
+//             const tabbar = link.parentElement.parentElement;
+//             const shopList = document.querySelector(".shop");
+//             const tabNumber = link.dataset.forTab;
 
-            const tabToActivate = shopList.querySelector(`li[data-tab="${tabNumber}"]`);
+//             const tabToActivate = shopList.querySelector(`li[data-tab="${tabNumber}"]`);
 
-            // HELP =============================
-            // CAN ONLY TOGGLE, DOESN'T WORK EXACTLY AS INTENDED
+//             // HELP =============================
+//             // CAN ONLY TOGGLE, DOESN'T WORK EXACTLY AS INTENDED
 
-            tabbar.querySelectorAll(".tabbar__link").forEach(link => {
-                link.classList.remove("tabbar__link--active");
+//             tabbar.querySelectorAll(".tabbar__link").forEach(link => {
+//                 link.classList.remove("tabbar__link--active");
 
-            });
+//             });
 
-            shopList.querySelectorAll(".shop__item").forEach(item => {
-                item.classList.remove("shop__item--active");
+//             shopList.querySelectorAll(".shop__item").forEach(item => {
+//                 item.classList.remove("shop__item--active");
 
-            });
+//             });
 
-            link.classList.add("tabbar__link--active");
-            tabToActivate.classList.add("shop__item--active");
-
-
-
-
-            // tabbar.querySelectorAll(`.tabbar__link--${tabNumber}`).forEach(link => {
-            //     link.classList.toggle(`tabbar__link--active`);
-            //     link.classList.toggle(`tabbar__link--active`);
-            //     link.classList.toggle(`tabbar__link--active`);
-                
-            // });
-
-            // shopList.querySelectorAll(`.shop__item--${tabNumber}`).forEach(shopItem => {
-            //     if(shopItem.classList.contains("shop__item--active")){
-            //         shopItem.classList.remove("shop__item--active");
-            //     } else{
-            //         shopItem.classList.add("shop__item--active");
-            //     }
-                
-
-            // });
+//             link.classList.add("tabbar__link--active");
+//             tabToActivate.classList.add("shop__item--active");
             
 
-        });
-    })
-}
+//         });
+//     })
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    setUpTabs();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     setUpTabs();
+// });
+
+
+const tabs = document.querySelectorAll(`[data-tab-target]`);
+
+tabs.forEach(tab => {
+tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    target.classList.add("active");
+})
+
+})
